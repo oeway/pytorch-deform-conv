68b723890476885d21e62296c31a787477773b39
Wei Ouyang <oeway007@gmail.com>
2017-04-04 23:17:18 +0200

    add transfer_weights

--- torch_deform_conv/utils.py
+++ torch_deform_conv/utils.py
@@ -1,1 +1,9 @@
-from __future__ import absolute_import, division
+import copy
+
+def transfer_weights(model_from, model_to):
+    wf = copy.deepcopy(model_from.state_dict())
+    wt = model_to.state_dict()
+    for k in wt.keys():
+        if not k in wf:
+            wf[k] = wt[k]
+    model_to.load_state_dict(wf)
