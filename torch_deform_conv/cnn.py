3f3f30a567256788f2770e629751a6c36c8ca9fb
Wei Ouyang <oeway007@gmail.com>
2017-04-05 00:47:08 +0200

    update DeformConvNet

--- torch_deform_conv/cnn.py
+++ torch_deform_conv/cnn.py
@@ -49,7 +49,7 @@ class ConvNet(nn.Module):
 class DeformConvNet(nn.Module):
     def __init__(self):
         super(DeformConvNet, self).__init__()
-
+        
         # conv11
         self.conv11 = nn.Conv2d(1, 32, 3, padding=1)
         self.bn11 = nn.BatchNorm2d(32)
@@ -75,73 +75,28 @@ class DeformConvNet(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv11(x))
         x = self.bn11(x)
-
+        
         x = self.offset12(x)
         x = F.relu(self.conv12(x))
         x = self.bn12(x)
-
+        
         x = self.offset21(x)
         x = F.relu(self.conv21(x))
         x = self.bn21(x)
-
+        
         x = self.offset22(x)
         x = F.relu(self.conv22(x))
         x = self.bn22(x)
-
+        
         x = F.avg_pool2d(x, kernel_size=[x.size(2), x.size(3)])
-        x = self.fc(x.view(x.size()[:2]))#
+        x = self.fc(x.view(x.size()[:2]))
         x = F.softmax(x)
         return x
 
-class DeformConvNet(nn.Module):
-    def __init__(self):
-        super(DeformConvNet, self).__init__()
-
-        # conv11
-        self.conv11 = nn.Conv2d(1, 32, 3, padding=1)
-        self.bn11 = nn.BatchNorm2d(32)
-
-        # conv12
-        self.offset12 = ConvOffset2D(32)
-        self.conv12 = nn.Conv2d(32, 64, 3, padding=1, stride=2)
-        self.bn12 = nn.BatchNorm2d(64)
-
-        # conv21
-        self.offset21 = ConvOffset2D(64)
-        self.conv21 = nn.Conv2d(64, 128, 3, padding= 1)
-        self.bn21 = nn.BatchNorm2d(128)
-
-        # conv22
-        self.offset22 = ConvOffset2D(128)
-        self.conv22 = nn.Conv2d(128, 128, 3, padding=1, stride=2)
-        self.bn22 = nn.BatchNorm2d(128)
-
-        # out
-        self.fc = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv11(x))
-        x = self.bn11(x)
-
-        x = self.offset12(x)
-        x = F.relu(self.conv12(x))
-        x = self.bn12(x)
-
-        x = self.offset21(x)
-        x = F.relu(self.conv21(x))
-        x = self.bn21(x)
-
-        x = self.offset22(x)
-        x = F.relu(self.conv22(x))
-        x = self.bn22(x)
-
-        x = F.avg_pool2d(x, kernel_size=[x.size(2), x.size(3)])
-        x = self.fc(x.view(x.size()[:2]))#
-        x = F.softmax(x)
-        return x
 
 def get_cnn():
     return ConvNet()
+
 
 def get_deform_cnn(trainable=True):
     model = DeformConvNet()
